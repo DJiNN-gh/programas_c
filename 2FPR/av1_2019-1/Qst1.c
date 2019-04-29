719761b92c8cd4c30a9bfffd7b8d6284212ff8d4
DJiNN <allanlopes@Allans-MacBook-Air.local>
2019-04-29 20:54:49 -0300

    Update Monday 29 April 20:55 MacB

---
+++ 2FPR/av1_2019-1/Qst1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+float recursor (float *array_f, unsigned int tam_ui);
+
+int main (void){
+
+
+return 0;}
+
+// Função que verifica se um vetor está ordenado crescentemente
+float recursor (float *array_f, unsigned int tam_ui, unsigned int *check_ui){
+
+	if (tam_ui > 0){
+
+		if (array_f[tam_ui-1] >= recursor(array_f, tam_ui-1)){
+
+			return array_f[tam-1];}
+
+		else
+			return FALSE;}
+	
+	*check_ui=TRUE;}
